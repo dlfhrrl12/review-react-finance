99405eb0112ebf9e291565ac632be29c37dd601a
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-03-21 14:42:22 +0900

    page 셋팅

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="container max-w-3xl">
+      <div className="w-[100px] h-[100px] bg-red-600"></div>
+    </main>
+  );
+};
+
+export default Home;
