7e96ea8574c785e3f087fe22671a0b8fb70a455f
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-03-21 20:00:17 +0900

    Home ui 디자인

---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,9 @@
+const ExpenseList = () => {
+  return (
+    <section className="rounded-2xl border shadow-lg p-5 ">
+      <div className="flex flex-col gap-[10px]"></div>
+    </section>
+  );
+};
+
+export default ExpenseList;
